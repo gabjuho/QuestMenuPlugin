51e86f008d29ac7c736a8abf6641f556d1f651c5
gabjuho <gabjuho@gmail.com>
2022-07-06 04:08:26 +0900

    feat: Add command JunaraQuest to fire conversation event about meina

--- src/main/java/com/gabjuho/QuestMenuPlugin/Main.java
+++ src/main/java/com/gabjuho/QuestMenuPlugin/Main.java
@@ -1,15 +1,17 @@
 package com.gabjuho.QuestMenuPlugin;
 
+import com.gabjuho.QuestMenuPlugin.commands.Command;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     @Override
     public void onEnable(){
-        getServer().getConsoleSender().sendMessage(ChatColor.GREEN+ "[System]: QuestMenu is enabled");
+        getServer().getConsoleSender().sendMessage(ChatColor.GREEN+ "[QuestMenu]: QuestMenu is enabled");
+        getCommand("JunaraQuest").setExecutor(new Command());
     }
     @Override
     public void onDisable(){
-        getServer().getConsoleSender().sendMessage(ChatColor.RED+ "[System]: QuestMenu is disabled");
+        getServer().getConsoleSender().sendMessage(ChatColor.RED+ "[QuestMenu]: QuestMenu is disabled");
     }
 }
--- src/main/java/com/gabjuho/QuestMenuPlugin/commands/Command.java
+++ src/main/java/com/gabjuho/QuestMenuPlugin/commands/Command.java
@@ -1,5 +1,10 @@
 package com.gabjuho.QuestMenuPlugin.commands;
 
+import org.betonquest.betonquest.BetonQuest;
+import org.betonquest.betonquest.api.config.QuestPackage;
+import org.betonquest.betonquest.config.Config;
+import org.betonquest.betonquest.exceptions.ObjectNotFoundException;
+import org.betonquest.betonquest.id.EventID;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -11,6 +16,17 @@ public class Command implements CommandExecutor {
         if(!(sender instanceof Player)) {
             sender.sendMessage("오직 플레이어만 명령어를 사용할 수 있습니다.");
             return true;
+        }
+        Player player = (Player) sender;
+
+        if(cmd.getName().equalsIgnoreCase("JunaraQuest")){
+            QuestPackage myPackage = Config.getPackages().get("WildQuest");
+            try {
+                EventID eventID = new EventID(myPackage, "talk_with_meina");
+                BetonQuest.event(player.getUniqueId().toString(),eventID);
+            } catch (ObjectNotFoundException e) {
+                e.printStackTrace();
+            }
         }
 
         return true;
